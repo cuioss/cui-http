8a03e7e0a9964d377194b93f09d2424d5f6b92bf
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2025-09-15 16:52:33 +0200

    Prepare project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module de.cuioss.template {
-    exports de.cuioss.template;
+module de.cuioss.http {
+    exports de.cuioss.http;
 
     requires static lombok;
 }
